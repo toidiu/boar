4cfd08acfe8dc61ef03edf8bb0f995bf9a1616bc
Apoorv Kothari <akothari@cloudflare.com>
2025-11-02 09:46:48 -0800

    wip.. attempting to run server.. debug

---
+++ src/error.rs
@@ -0,0 +1,6 @@
+pub type Result<T, E = BoarError> = core::result::Result<T, E>;
+
+#[derive(Debug)]
+pub enum BoarError {
+    Script,
+}
--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,57 @@ mod error;
 fn main() -> Result<()> {
     println!("Hello, world!");
 
-    parse_user_input();
+    // Cli
+    let plan = parse_user_input();
 
+    // Network
+    delete_network()?;
     setup_network()?;
 
-    run_server();
+    // Run
+    run_server(&plan);
+    run_client(&plan);
 
-    run_client();
-
+    // Data
     collect_stats();
-
     analyze_stats();
 
+    // Report
     gen_report();
 
     Ok(())
 }
 
-fn parse_user_input() {}
+struct ExecutionPlan {
+    server_location: String,
+    client_location: String,
+}
+
+fn parse_user_input() -> ExecutionPlan {
+    ExecutionPlan {
+        server_location: "/Users/akothari/projects/quiche/target/debug/examples/async_http3_server"
+            .to_string(),
+        client_location: "/Users/akothari/projects/quiche/target/debug/examples/quiche-client"
+            .to_string(),
+    }
+}
+
+fn delete_network() -> Result<()> {
+    let res = Command::new("sh")
+        .arg("-c")
+        .arg("./scripts/test.sh")
+        .stdout(Stdio::piped())
+        .output()
+        .unwrap();
+
+    println!("{:?}", str::from_utf8(&res.stdout));
+
+    if res.status.success() {
+        Ok(())
+    } else {
+        Err(BoarError::Script)
+    }
+}
 
 fn setup_network() -> Result<()> {
     let res = Command::new("sh")
@@ -44,9 +77,24 @@ fn setup_network() -> Result<()> {
     }
 }
 
-fn run_server() {}
+fn run_server(plan: &ExecutionPlan) {
+    let server = &plan.server_location;
+    println!("{:?}", server);
 
-fn run_client() {}
+    let res = Command::new("sh")
+        .arg("-c")
+        .arg(server)
+        .arg("--address")
+        .arg("0.0.0.0:8888")
+        .status()
+        // .stdout(Stdio::piped())
+        // .output()
+        .unwrap();
+
+    // println!("{:?}", str::from_utf8(&res.stdout));
+}
+
+fn run_client(plan: &ExecutionPlan) {}
 
 fn collect_stats() {}
 
